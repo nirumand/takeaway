cdab5beacdb142ae4de876f058f7a8f380560f4a
Reza Nirumand <nirumand@web.de>
2018-08-09 10:53:30 +0200

    everything is working

--- employee_service/src/main/java/com/takeaway/challenge/employeeservice/service/KafkaService.java
+++ employee_service/src/main/java/com/takeaway/challenge/employeeservice/service/KafkaService.java
@@ -1,13 +1,20 @@
 package com.takeaway.challenge.employeeservice.service;
 
 import com.takeaway.challenge.employeeservice.event.BusinessEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Service
 public class KafkaService {
+	private static final Logger logger = LogManager.getLogger(KafkaService.class);
 	private KafkaTemplate<String, String> kafkaTemplate;
 	private String kafkaTopic;
 
@@ -20,6 +27,15 @@ public class KafkaService {
 	}
 
 	public void produce(BusinessEvent event) {
-		kafkaTemplate.send(kafkaTopic, event.toString());
+		try {
+			kafkaTemplate.send(kafkaTopic, event.toString()).get(100, TimeUnit.MILLISECONDS);
+			logger.info("event Sent: [%s]", event.toString());
+
+		} catch (ExecutionException e) {
+			logger.error("unable to send event= [%s]", event.toString(), e);
+
+		} catch (TimeoutException | InterruptedException e) {
+			logger.error("unable to send event= [%s]", event.toString(), e);
+		}
 	}
 }
--- event_service/src/main/java/com/takeaway/challenge/eventservice/service/KafkaService.java
+++ event_service/src/main/java/com/takeaway/challenge/eventservice/service/KafkaService.java
@@ -6,56 +6,28 @@ import com.takeaway.challenge.eventservice.repository.EventRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Captures events published to the configured topic and persists it to the database.
  * If there is any problem with parsing the event or persisting, an error will be logged.
  */
 @Service
-
 public class KafkaService {
 	private static final Logger logger = LogManager.getLogger(KafkaService.class);
-	private KafkaTemplate<String, String> kafkaTemplate;
-	private String kafkaTopic;
-
 	private ObjectMapper mapper;
 	private EventRepository eventRepository;
 
 	@Autowired
-	public KafkaService(KafkaTemplate<String, String> kafkaTemplate,
-						@Value("${takeaway.kafka.topic}") String kafkaTopic,
-						EventRepository eventRepository) {
-		this.kafkaTemplate = kafkaTemplate;
+	public KafkaService(EventRepository eventRepository) {
 		this.mapper = new ObjectMapper();
 		this.eventRepository = eventRepository;
-		this.kafkaTopic = kafkaTopic;
 	}
 
-	public void produce(BusinessEvent event) {
-		try {
-			kafkaTemplate.send(kafkaTopic, event.toString()).get(100, TimeUnit.MILLISECONDS);
-			logger.info("event Sent: [%s]", event.toString());
-
-		} catch (ExecutionException e) {
-			logger.error("unable to send event= [%s]", event.toString(), e);
-
-		} catch (TimeoutException | InterruptedException e) {
-			logger.error("unable to send event= [%s]", event.toString(), e);
-		}
-	}
-
-	//	@KafkaListener(topics = "${takeaway.kafka.topic}")
-	@KafkaListener(topics = "codechallenge")
+	@KafkaListener(topics = "${takeaway.kafka.topic}")
 	public void onEventInCodeChallenge(String event) {
 		try {
 			logger.debug("Event received : [{}]", event);
